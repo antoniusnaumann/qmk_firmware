cd4061bb06643065ddfc11aeca969320f77feec2
Antonius Naumann <git@antonius.dev>
2023-04-04 12:02:02 +0200

    Update minidox keymap and config for homerow mods

--- keyboards/maple_computing/minidox/config.h
+++ keyboards/maple_computing/minidox/config.h
@@ -25,7 +25,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
-
+#define QUICK_TAP_TERM 20
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
 /* disable debug print */
 //#define NO_DEBUG
 
